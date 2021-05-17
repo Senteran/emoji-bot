ef67fc9c3d09af50847226d81ed191f722e7cddc
Senteran <macienias@gmail.com>
2021-05-17 13:47:32 +0200

    Naprawiono granie z yt

--- modules/functions.py
+++ modules/functions.py
@@ -386,5 +386,5 @@ async def beast_mode_off(client):
 
 async def reply_to_message(message, content):
     await message.reply(content)
-    message.channel.send('Image not found')
+    await message.channel.send('Image not found')
     return
