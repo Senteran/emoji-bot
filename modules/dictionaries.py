45fa02ddfe9fafea4c520d125b0e80764e685217
Senteran <macienias@gmail.com>
2021-08-08 13:13:30 +0200

    Zamieniono katakane na hiragane za prosba kato

--- modules/dictionaries.py
+++ modules/dictionaries.py
@@ -320,18 +320,18 @@ names_to_nick = {
 }
 
 names_to_custom_nick = {
-    'Senteran': 'ジャック',
-    'Krupier' : 'ジョン',
-    'Kuchta' : 'マリウス',
-    'Lakey' : 'ピーター',
-    'Sebek' : 'セバスチャン',
-    'Kato' : 'カミル',
-    'ertymaster' : 'アダム',
-    'exeos' : 'ポール 野ウサギ',
-    'KFrankVegeta' : 'カロリナ',
-    'Hot Dog Nibba' : 'アラン',
-    'Aleksandra Pawlik' : 'アレクサンダー 収納スペース',
-    'Frido' : 'ポール クリン'
+    'Senteran': 'じゃっく',
+    'Krupier' : 'じょん',
+    'Kuchta' : 'まりうす',
+    'Lakey' : 'ぴーたー',
+    'Sebek' : 'せばすちゃん',
+    'Kato' : 'かみる',
+    'ertymaster' : 'あだむ',
+    'exeos' : 'ぽーる 野うさぎ',
+    'KFrankVegeta' : 'かろりな',
+    'Hot Dog Nibba' : 'あらん',
+    'Aleksandra Pawlik' : 'あれくさんだー 収納すぺーす',
+    'Frido' : 'ぽーる くりん'
 }
 
 deletion_responses = [
