657d70035abcab9edde56f2d9711b4d72ed76801
Senteran <macienias@gmail.com>
2022-02-06 21:55:19 +0100

    sraker

--- modules/dictionaries.py
+++ modules/dictionaries.py
@@ -161,7 +161,8 @@ send_library = {
     'w co gramy': 'Wykres dostępnych gier jest dostępny tu: \
         https://docs.google.com/spreadsheets/d/1BefpD-0jU_2GDn-yyG8_-HUVYyUBxnoK86ntY4O6Uo4/edit#gid=0',
     'cum trilogy': '-p welcome to the cumzone\n-p your cum wont last\n-p heir to the cum throne',
-    'jak wielki jest penis seby?': '40 centymetrów flacid'
+    'jak wielki jest penis seby?': '40 centymetrów flacid',
+    'emoji sraker' : 'https://www.youtube.com/watch?v=eWb78tJtYRk'
 }
 
 music_library = {
