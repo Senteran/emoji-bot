89af7bbe370f1cb5f942493214cbe1a3ded86959
Senteran <43209971+Senteran@users.noreply.github.com>
2022-03-11 09:31:46 +0100

    fix file

--- modules/functions.py
+++ modules/functions.py
@@ -949,7 +949,7 @@ async def delete_message_by_id(message, client):
     await mess.delete()
 
 async def paper_janka(message):
-    await message.channel.send(file=discord.file('src/paper_janka_1.jpg'))
-    await message.channel.send(file=discord.file('src/paper_janka_2.jpg'))
-    await message.channel.send(file=discord.file('src/paper_janka_3.jpg'))
-    await message.channel.send(file=discord.file('src/paper_janka_4.jpg'))
+    await message.channel.send(file=discord.File('src/paper_janka_1.jpg'))
+    await message.channel.send(file=discord.File('src/paper_janka_2.jpg'))
+    await message.channel.send(file=discord.File('src/paper_janka_3.jpg'))
+    await message.channel.send(file=discord.File('src/paper_janka_4.jpg'))
